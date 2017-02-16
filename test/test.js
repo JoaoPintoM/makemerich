742140a92bb2736b5673a8812c121ff9a092301b
Joao Pinto <pinto.joao@outlook.com>
2017-02-16 11:17:10 +0100

    eslint describe and it rule for tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
+/* global describe it */
 import assert from 'assert'
 
-describe('Array', function() {
+describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+      assert.equal(-1, [1, 2, 3].indexOf(4))
+    })
+  })
+})
